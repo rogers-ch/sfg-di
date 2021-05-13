1ced363145633ede6653147a4dd4f2bbce4222f8
rogers-ch <crogers25@mail.greenriver.edu>
2021-05-13 11:49:30 -0700

    Video 73 completed - Java Config Example

--- src/main/java/guru/springframework/sfgdi/services/SetterInjectedGreetingService.java
+++ src/main/java/guru/springframework/sfgdi/services/SetterInjectedGreetingService.java
@@ -1,11 +1,8 @@
 package guru.springframework.sfgdi.services;
 
-import org.springframework.stereotype.Service;
-
 /**
  * Created by cr on 04/23/2021
  */
-@Service
 public class SetterInjectedGreetingService implements GreetingService {
     @Override
     public String sayGreeting() {
