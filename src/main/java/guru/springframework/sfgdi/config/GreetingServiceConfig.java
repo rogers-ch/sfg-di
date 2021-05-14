ebf15a281c7614129012b0d16c5c5a7bc95b06ba
rogers-ch <crogers25@mail.greenriver.edu>
2021-05-14 12:28:17 -0700

    Video 90 completed - Spring Boot Application.properties

--- src/main/java/guru/springframework/sfgdi/config/GreetingServiceConfig.java
+++ src/main/java/guru/springframework/sfgdi/config/GreetingServiceConfig.java
@@ -12,7 +12,6 @@ import org.springframework.context.annotation.*;
 /**
  * created by cr on 05/13/2021
  */
-@PropertySource("classpath:datasource.properties")
 @ImportResource("classpath:sfgdi-config.xml")
 @Configuration
 public class GreetingServiceConfig {
